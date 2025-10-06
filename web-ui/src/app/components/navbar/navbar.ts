a2709a69eeb47401076f11afa779976c4f2e2878
Justinas Ribakauskas <j.ribakau@gmail.com>
2025-10-06 18:23:04 +0300

    CB-6.1 added menu bar

---
+++ web-ui/src/app/components/navbar/navbar.ts
@@ -0,0 +1,24 @@
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+    selector: 'app-navbar',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './navbar.html',
+    styleUrls: ['./navbar.scss']
+})
+export class NavbarComponent {
+    @Input() userName?: string;
+    @Input() userAvatar?: string;
+    @Output() logout = new EventEmitter<void>();
+    @Output() settings = new EventEmitter<void>();
+
+    onLogout(): void {
+        this.logout.emit();
+    }
+
+    onSettings(): void {
+        this.settings.emit();
+    }
+}
